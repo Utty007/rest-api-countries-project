537ff6bfc5572514bd637cc2745e2356017dfb84
Kayode Uthman <88583385+Utty007@users.noreply.github.com>
2023-10-07 23:19:19 -0700

    Color mode change

--- src/Store/ui-slice.jsx
+++ src/Store/ui-slice.jsx
@@ -1,14 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+function detectSystemColorMode() {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  return prefersDarkMode;
+}
+
+
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { lightModeActive: false, navBarIsVisible: false },
+  initialState: { lightModeActive: !detectSystemColorMode() },
   reducers: {
     toggle(state) {
       state.lightModeActive = !state.lightModeActive;
     }
   },
 });
+
 
 export const uiActions = uiSlice.actions;
 
